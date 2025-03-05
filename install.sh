a8511be12ca4ab41412ef886bd80b0034d7a5ce2
elsa <elsa@Inception.myguest.virtualbox.org>
2025-03-05 21:38:12 +0100

    Add Docker

--- install.sh
+++ install.sh
@@ -21,6 +21,15 @@ else
         trap 'echo -e "${green}success${reset}"' EXIT
 fi
 
+: <<'END_COMMENT'
+/********************************/
+/*				*/
+/*	     LINUX PART		*/
+/*				*/
+/********************************/
+END_COMMENT
+
+
 # Install sudo
 # if dpkg -l | grep -q sudo; then
 if ! sudo --version &>/dev/null; then
@@ -124,3 +133,54 @@ apt-get remove wget
 apt-get remove openssh-server
 apt-get remove make
 END_COMMENT
+
+: <<'END_COMMENT'
+/********************************/
+/*				*/
+/*	    DOCKER PART		*/
+/*				*/
+/********************************/
+END_COMMENT
+
+# Uninstall all conflicting packages
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Set up Docker's apt repository
+	# Install ca-certificates
+if ! which ca-certificates &>/dev/null; then
+	echo -e "\n${blue}Installing ca-certificates${reset}"
+	sudo apt-get update
+	sudo apt-get install ca-certificates curl
+	echo -e "${green}Success:${reset} ca-certificates is installed"
+else
+	echo -e "Docker's GPG key is already installed"
+fi
+
+	# Add Docker's official GPG key
+if [ ! -f /etc/apt/keyrings/docker.asc ]; then
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+	# Add the repository to Apt sources
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+	echo -e "${green}Success:${reset} Docker's GPG key is installed"
+else
+	echo -e "Docker's GPG key is already installed"
+fi
+
+# Install docker
+if ! which docker &>/dev/null; then
+	echo -e "\n${blue}Installing Docker${reset}"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	echo -e "${green}Success:${reset} Docker is installed"
+else
+	echo -e "Docker is already installed"
+fi
+
+# Verify that the installation is successful
+sudo docker run hello-world
