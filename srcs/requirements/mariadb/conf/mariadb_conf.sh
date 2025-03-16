d22c3e6aee1789255fd208eb48622d5d6a96eece
elsa <elsa@Inception.myguest.virtualbox.org>
2025-03-16 03:39:06 +0100

    Mariadb server runs

--- srcs/requirements/mariadb/conf/mariadb_conf.sh
+++ srcs/requirements/mariadb/conf/mariadb_conf.sh
@@ -7,9 +7,20 @@ blue='\e[34m'
 reset='\e[0m'  
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
+	# Change owner: define mysql as owner and group
  	chown -R mysql:mysql /var/lib/mysql /run/mysqld
+
+	# Change permissions
 	chmod 755 /var/lib/mysql /run/mysqld
+
+	# Init database
 	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+	# Check if db is safe
+	tfile=`mktemp`
+	 if [ ! -f "$tfile" ]; then
+		return 1
+	fi
 	echo -e "${green}Database created${reset}" 
 #	mariadb-admin shutdown
 #	mysql -u root -e "SHUTDOWN;"
