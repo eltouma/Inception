d31d44d6e31a354d478af716db12462b4273f12d
elsa <elsa@Inception.myguest.virtualbox.org>
2025-03-16 02:16:39 +0100

    Mariadb server runs

--- srcs/requirements/mariadb/conf/mariadb_conf.sh
+++ srcs/requirements/mariadb/conf/mariadb_conf.sh
@@ -1,4 +1,21 @@
 #!/bin/ash
 
+# Colors 
+green='\e[32m'                                      
+red='\e[31m'                                        
+blue='\e[34m'                                       
+reset='\e[0m'  
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+ 	chown -R mysql:mysql /var/lib/mysql /run/mysql
+	chmod 755 /var/lib/mysql /run/mysql
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+	echo -e "${green}Database created${reset}" 
+#	mariadb-admin shutdown
+#	mysql -u root -e "SHUTDOWN;"
+# 	rc-service mariadb stop
+fi
+
 echo "coucou"
-# mysql -u root
+exec "$@"
+
