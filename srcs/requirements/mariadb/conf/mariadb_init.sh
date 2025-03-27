8b06e0617512fb6d5097ba1c4dcf5a466524ac2c
root <root@eltouma.42.fr>
2025-03-27 14:02:58 +0100

    pb with user. Solved with David

--- srcs/requirements/mariadb/conf/mariadb_init.sh
+++ srcs/requirements/mariadb/conf/mariadb_init.sh
@@ -48,3 +48,38 @@ END_COMMENT
 exec "$@"
 
 # HEALTHCHECK
+
+
+####################### DAVID ######################
+#!/bin/sh
+
+# Initialiser le répertoire de données si nécessaire
+[ ! -d "/var/lib/mysql/mysql" ] && mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null
+
+# Démarrer le serveur MariaDB temporaire
+mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
+
+# Attendre que MariaDB soit prêt
+for i in $(seq 1 10); do
+    mariadb -u root -e "SELECT 1" > /dev/null 2>&1 && break
+    sleep 1
+done
+
+# Configurer MariaDB (root, base de données et utilisateur)
+mariadb -u root <<-EOSQL
+    SET @@SESSION.SQL_LOG_BIN=0;
+    DELETE FROM mysql.user WHERE User='';
+    CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${WP_PASSWORD}';                       
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+    CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';                 
+    GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+    FLUSH PRIVILEGES;
+EOSQL
+
+# Arrêter le serveur temporaire                                                                
+mysqladmin shutdown -u root --password="${MYSQL_ROOT_PASSWORD}"                                
+
+# Lancer MariaDB normalement                                                                   
+exec "$@"
+
