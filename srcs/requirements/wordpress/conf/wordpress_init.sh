1150cc8720130255344477f9511ed95931f3ce8f
root <root@eltouma.42.fr>
2025-03-31 19:10:28 +0200

    TLS ok

--- srcs/requirements/wordpress/conf/wordpress_init.sh
+++ srcs/requirements/wordpress/conf/wordpress_init.sh
@@ -1,28 +1,5 @@
 #!/bin/sh
 
-: <<'END_COMMENT'
-if [ ! -f wp-cli.phar ];
-then
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-fi
-
-if ! php83 wp-cli.phar core is-installed;
-then
-    php83 wp-cli.phar core download --version=8.3
-    php83 wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb:3306
-    php83 wp-cli.phar core install --url="localhost" --title="title" --admin_user="admin"--admin_password="test" --admin_email="elsa.touma@test.fr";
-
-#    php83 wp-cli.phar user create --role=author $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWD
-    chown -R nobody:nobody /srv/www/wordpress
-fi
-
-#sed -i -E "s/^listen = 127.0.0.1/listen = 0.0.0.0/" /etc/php83/php-fpm.d/www.conf
-#sed -i -E "s/;ping.path/ping.path/" /etc/php83/php-fpm.d/www.conf
-#sed -i -E "s/;ping.response/ping.response/" /etc/php83/php-fpm.d/www.conf
-
-exec "$@"
-END_COMMENT
-
 green='\e[92m'
 red='\e[91m'
 yellow='\e[93m'
