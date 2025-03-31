fa0f45bf5fc1873f52046ba542142b157e12d5f1
root <root@eltouma.42.fr>
2025-04-01 00:21:25 +0200

    It works bitch

--- srcs/requirements/wordpress/conf/wordpress_init.sh
+++ srcs/requirements/wordpress/conf/wordpress_init.sh
@@ -57,7 +57,7 @@ php83 wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB
 #php83 wp-cli.phar db create
 
 	echo -e "${red}config installation${reset}"
-php83 wp-cli.phar core install --url=localhost --title=inception --admin_user=$DB_USER --admin_password=$DB_ROOT_PASSWORD --admin_email=test@test.com --path='/var/www/wordpress' --allow-root
+php83 wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$DB_USER --admin_password=$DB_ROOT_PASSWORD --admin_email=test@test.com --path='/var/www/wordpress' --allow-root
 
 	echo -e "${red}config installed${reset}"
 #php83 wp-cli.phar core config --dbhost=mariadb:3306 --dbname=mariadb --dbuser=elsa --dbpass=secret 
@@ -65,7 +65,7 @@ php83 wp-cli.phar user create --role=author elsauser elsa@test.com --user_pass=$
 chown -R nobody:nobody /var/www/wordpress
 fi
 
-sed -i -E "s/;listen = 127.0.0.1/listen = 0.0.0.0/" /etc/php83/php-fpm.d/www.conf
+sed -i -E "s/^listen = 127.0.0.1/listen = 0.0.0.0/" /etc/php83/php-fpm.d/www.conf
 # sed -i -E "s/;ping.path/ping.path/" /etc/php83/php-fpm.d/www.conf
 #sed -i -E "s/;ping.response/ping.response/" /etc/php83/php-fpm.d/www.conf
 
